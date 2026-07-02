4e4ada20e6f0aac72ffb437b44c8e99e9654aacb
Parth <partharora2632@gmail.com>
2026-07-02 14:01:18 +0530

    Add custom avatar favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -57,6 +57,9 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: 'en'
       },
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' }
+      ],
       script: []
     }
   },
